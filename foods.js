26044ca96c0658d024d58fc7df30000b9c413aa3
Benjamin Pearson <bpearson@arklegaudit.gov>
2018-11-07 10:23:14 -0600

    first

---
+++ foods.js
@@ -0,0 +1,8 @@
+var foods = [
+    {
+        'name': 'granny smith apple',
+        'flavorLoc':
+        {'savory':1,'sweet':7,'salty':1,'numbing':2,'pungent':1,'cooling':3,'sour':6,'bitter':2,'astringent':5,'chalky':2,'fatty':1,'hearty':1,'cold':10,'starchy':8},
+        'url':'https://www.nutritionvalue.org/Apples%2C_with_skin%2C_granny_smith%2C_raw_nutritional_value.html'
+    }
+];
